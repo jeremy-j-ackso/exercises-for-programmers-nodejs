17120ef065ec6c1c5a65688fd50f745bbc1e3c2c
Jeremy Jackson <jeremy_j@ackso.net>
2017-12-20 21:39:09 -0700

    Added eslint and fixed the files for linting errors.

--- ch1/index.js
+++ ch1/index.js
@@ -1,6 +1,6 @@
 function tipCalculator(billAmount, tipRate) {
-  var bill = billAmount
-  var tipPerc = tipRate
+  let bill = billAmount
+  let tipPerc = tipRate
 
   if (typeof bill !== 'number') {
     throw new Error('billAmount must be a number, either float or integer.')
@@ -8,7 +8,7 @@ function tipCalculator(billAmount, tipRate) {
 
   if (typeof tipPerc !== 'number') {
     throw new Error('tipRate must be a number, either float or integer.')
-  } 
+  }
 
   if (bill <= 0) {
     throw new Error('billAmount must be greater than 0')
@@ -19,20 +19,21 @@ function tipCalculator(billAmount, tipRate) {
   }
 
   if (tipPerc < 1) {
-    console.warn('You entered a tipRate less than 1.\n',
+    console.warn(
+      'You entered a tipRate less than 1.\n',
       'The program will still function with this value, however you should know that it ',
       'expects the tipRate to be in the form of, for example 15%, so you would enter 15 ',
-      'not 0.15.\n'
+      'not 0.15.\n',
     )
   }
 
-  tipPerc = tipPerc / 100
+  // tipPerc = tipPerc / 100
+  tipPerc /= 100
 
-  var tip = parseFloat((bill * tipPerc).toFixed(2))
-  var total = parseFloat((bill + tip).toFixed(2))
+  let tip = parseFloat((bill * tipPerc).toFixed(2))
+  let total = parseFloat((bill + tip).toFixed(2))
 
-  return {tip, total}
-
+  return { tip, total }
 }
 
 module.exports = tipCalculator
--- test/ch1.test.js
+++ test/ch1.test.js
@@ -1,33 +1,35 @@
-var expect = require('expect.js')
-var tipCalculator = require('../ch1')
+/* eslint no-undef: "off" */
 
-describe('tipCalculator', function() {
-  var ref_tests = [
-    {args: [1, 15], expected: {tip: 0.15, total: 1.15}},
-    {args: [15, 20], expected: {tip: 3.00, total: 18.00}},
-    {args: [15.30, 18.5], expected: {tip: 2.83, total: 18.13}},
+let expect = require('expect.js')
+let tipCalculator = require('../ch1')
+
+describe('tipCalculator', () => {
+  let ref_tests = [
+    { args: [1, 15], expected: { tip: 0.15, total: 1.15 } },
+    { args: [15, 20], expected: { tip: 3.00, total: 18.00 } },
+    { args: [15.30, 18.5], expected: { tip: 2.83, total: 18.13 } },
   ]
-  it('correctly outputs values equal to reference', function() {
-    ref_tests.forEach(function(test) {
+  it('correctly outputs values equal to reference', () => {
+    ref_tests.forEach((test) => {
       expect(tipCalculator(test.args[0], test.args[1])).to.eql(test.expected)
     })
   })
 
-  var err_tests = [
-    {args: ['one', 15], throws: 'billAmount must be a number, either float or integer.'},
-    {args: [1, 'fifteen'], throws: 'tipRate must be a number, either float or integer.'},
-    {args: ['one', 'fifteen'], throws: 'billAmount must be a number, either float or integer.'},
-    {args: [0, 15], throws: 'billAmount must be greater than 0'},
-    {args: [-12, 15], throws: 'billAmount must be greater than 0'},
-    {args: [12, -15], throws: 'tipRate must be greater than 0'},
-    {args: [12, 0], throws: 'tipRate must be greater than 0'},
-    {args: [undefined, 0], throws: 'billAmount must be greater than 0'},
-    {args: [0, undefined], throws: 'tipRate must be greater than 0'},
-    {args: [undefined, undefined], throws: 'billAmount must be greater than 0'},
-    {args: [null, null], throws: 'billAmount must be greater than 0'},
+  let err_tests = [
+    { args: ['one', 15], throws: 'billAmount must be a number, either float or integer.' },
+    { args: [1, 'fifteen'], throws: 'tipRate must be a number, either float or integer.' },
+    { args: ['one', 'fifteen'], throws: 'billAmount must be a number, either float or integer.' },
+    { args: [0, 15], throws: 'billAmount must be greater than 0' },
+    { args: [-12, 15], throws: 'billAmount must be greater than 0' },
+    { args: [12, -15], throws: 'tipRate must be greater than 0' },
+    { args: [12, 0], throws: 'tipRate must be greater than 0' },
+    { args: [undefined, 0], throws: 'billAmount must be greater than 0' },
+    { args: [0, undefined], throws: 'tipRate must be greater than 0' },
+    { args: [undefined, undefined], throws: 'billAmount must be greater than 0' },
+    { args: [null, null], throws: 'billAmount must be greater than 0' },
   ]
-  it('throw on invalid inputs', function() {
-    err_tests.forEach(function(test) {
+  it('throw on invalid inputs', () => {
+    err_tests.forEach((test) => {
       expect(tipCalculator).withArgs(test.args[0], test.args[1]).to.throwError(test.throws)
     })
   })
